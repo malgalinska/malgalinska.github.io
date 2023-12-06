d5a38f4e1ee2724c5ce8a39ce09958ac6ffd662c
Małgorzata Galińska <malgosia_biegala@o2.pl>
2023-12-06 03:34:38 +0100

    Add responsive layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -12,19 +12,17 @@ const { Title } = Typography;
 function Home() {
   return (
     <div id='home' style={{ minHeight: '100vh', paddingTop: '20vh' }}>
-      <Row style={{ minHeight: '70vh', margin: '16px'}}>
-        <Col span={8}>
-          <div id='profile_picture_container'>
-            <img src={rose_profile}/>
-            <img src={green_profile}/>
-            <img src={true_profile}/>
-          </div>
-        </Col>
-        <Col span={16} style={{textAlign: 'center'}}>
-          <Title id='title'>  Małgorzata Galińska </Title>
-          <Title level={3}> Software Developer </Title>
-        </Col>
-      </Row>
+      <Flex justify='center' align='start' wrap="wrap" style={{ minHeight: '70vh', width: '100%'}}>
+        <div id='profile_picture_container' style={{minWidth: '320px'}}>
+          <img src={rose_profile}/>
+          <img src={green_profile}/>
+          <img src={true_profile}/>
+        </div>
+        <div style={{minWidth: '320px'}}>
+          <Title id='title'>  Małgorzata<br/>Galińska </Title>
+          <Title id='subtitle' level={3}> Software Developer </Title>
+        </div>
+      </Flex>
       <Flex wrap='wrap' gap='small' justify='center'>
         <Button shape='round' href='https://www.linkedin.com/in/malgorzata-galinska/' target='_blank'>LinkedIn</Button>
         <a href={cv_file} download='Malgorzata_Galinska_Resume' target='_blank'>
